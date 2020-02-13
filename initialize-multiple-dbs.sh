9daf32871437ed27e444a605814dd9ae3572eb1a
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-02-14 00:18:42 +0800

    Working script

--- initialize-multiple-dbs.sh
+++ initialize-multiple-dbs.sh
@@ -4,20 +4,23 @@ set -u
 
 function create_db() {
   local db=$1
-  local is_user=$([ $MYSQL_USER = $db ] && echo 1 || echo 0)
+  local is_env_user=$([ $MYSQL_USER = $db ] && echo "2" || echo "0")
   local pass=$([ $MYSQL_USER = $db ] && echo "$MYSQL_PASS" || echo "$db")
   mysql -v -u root --password=$MYSQL_ROOT_PASSWORD <<-EOSQL
       -- create database
       CREATE DATABASE IF NOT EXISTS $db;
 
-      -- create user and grant usage permission
-      IF $is_user = 0 THEN
-        CREATE USER '$db' IDENTIFIED BY '$pass' $$
-        GRANT USAGE ON *.* TO '$db'@localhost IDENTIFIED BY '$pass' $$
-      END IF;
-
-      -- grant user access to database
-      GRANT ALL PRIVILEGES ON $db.* TO '$db'@localhost;
+      -- create user and grant permissions
+      DELIMITER $$
+        IF $is_env_user > 0 THEN
+          GRANT ALL PRIVILEGES ON $db.* TO '$db'@'%';
+        ELSE
+          CREATE USER '$db' IDENTIFIED BY '$pass';
+          GRANT USAGE ON *.* TO '$db'@localhost IDENTIFIED BY '$pass';
+          GRANT ALL PRIVILEGES ON $db.* TO '$db'@localhost;
+        END IF;
+      $$
+      DELIMITER ;
 
       -- apply permissions
       FLUSH PRIVILEGES;
@@ -25,7 +28,7 @@ EOSQL
 }
 
 if [ -n "$MYSQL_MULTIPLE_DATABASES" ]; then
-  echo "Creating multiple databases"
+  echo "[IN PROGRESS] Creating multiple databases"
   for db in $(echo $MYSQL_MULTIPLE_DATABASES | tr ',' ' '); do
     create_db $db
   done
